7eb4bab4bb9e60ec0126e0e7c5399c5fb266a067
abdiabbaz <abdiabbas@outlook.dk>
2024-08-27 13:39:23 +0200

    Added a class with some properties and a constructor

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ClassRoomNet60;
+
+
+
+Student student = new Student("Abdi",7,1);
+
+Console.WriteLine(student);
