a11139f42cfe38ae78bccd33b4e0d0b043fd27ab
abdiabbaz <abdiabbas@outlook.dk>
2024-08-27 13:47:35 +0200

    Added a classroom with some properties

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        public string? Name { get; set; }
+
+        public List<Student> Students { get; set; } 
+
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+            Name = "";
+            Students = new List<Student>();
+            SemesterStart = DateTime.Now;
+        }
+
+    }
+}
